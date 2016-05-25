77628fe000217ce4a14b3a7a424b50075bffc14b
Bart de Jonge <B.A.P.deJonge@student.tudelft.nl>
2016-05-25 21:32:32 +0200

    process code review comments

--- client_app/js/index.js
+++ client_app/js/index.js
@@ -1,15 +1,6 @@
 // eslint-disable-next-line no-undef
 $(document).ready(() => {
-    // const socket = io(); // eslint-disable-line no-undef
-    // console.log(socket.toString());
-    // console.log("hoi");
-    // $.ajax("/timeline/timeline-data").done((res) => {
-    //     console.log("alsdkjfalskdfj")
-    //     console.log(res);
-    //     var block = res.cameraTimelines[0];
-    //     console.log(block);
-    //     var html = '<timetable-block block=' + block + '></timetable-block>';
-    //     console.log(block);
-    //     $(".timetable-blocks").append(html);
-    // });
+    // Socket io boilerplate/temporary code
+    const socket = io(); // eslint-disable-line no-undef
+    console.log(socket.toString());
 });
