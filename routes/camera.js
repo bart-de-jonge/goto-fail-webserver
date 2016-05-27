a4ef2a3fb3bfcee0fd142fe8933cd5be06bdac09
Martijn Janssen <martijn9612@gmail.com>
2016-05-27 12:18:02 +0200

    Add panning and tilting of camera

--- routes/camera.js
+++ routes/camera.js
@@ -5,29 +5,113 @@ import http from "http";
 router.get("/save/:id(\\d\\d?)/", (req, res) => {
     let id = req.params.id;
     if (id.length === 1) {
-        id = '0' + id;
+        id = `0${id}`;
     }
     http.get({
-        host: '192.168.10.101',
-        path: '/cgi-bin/aw_ptz?cmd=%23M' + id + '&res=1'
-    }, (res) => {
-        console.log(res);
+        host: "192.168.10.101",
+        path: `/cgi-bin/aw_ptz?cmd=%23M${id}&res=1`,
+    }, (response) => {
+        if (response.statusCode === 200) {
+            res.status(200).json({ success: true, message: `Saved preset ${id}.` });
+        } else {
+            res.status(504).json({ success: false, message: `Unable to save preset ${id}.` });
+        }
     });
-    res.json({succes: true, message: "saved preset " + id });
 });
 
-router.get("/recall/:id(\\d\\d)/", (req, res) => {
+router.get("/recall/:id(\\d\\d?)/", (req, res) => {
     let id = req.params.id;
     if (id.length === 1) {
-        id = '0' + id;
+        id = `0${id}`;
     }
     http.get({
-        host: '192.168.10.101',
-        path: '/cgi-bin/aw_ptz?cmd=%23R' + id + '&res=1'
-    }, (res) => {
-        console.log(res);
+        host: "192.168.10.101",
+        path: `/cgi-bin/aw_ptz?cmd=%23R${id}&res=1`,
+    }, (response) => {
+        if (response.statusCode === 200) {
+            res.status(200).json({ success: true, message: `Recalled preset ${id}.` });
+        } else {
+            res.status(504).json({ success: false, message: `Unable to recall preset ${id}.` });
+        }
     });
-    res.json({succes: true, message: "recalled preset " + id });
+});
+
+router.get("/stop", (req, res) => {
+    http.get({
+        host: "192.168.10.101",
+        path: "/cgi-bin/aw_ptz?cmd=%23PTS5050&res=1",
+    }, (response) => {
+        if (response.statusCode === 200) {
+            res.status(200).json({ success: true, message: "Stopped camera." });
+        } else {
+            res.status(504).json({ success: false, message: "Unable to stop camera." });
+        }
+    });
+});
+
+router.post("/pan", (req, res) => {
+    const body = req.body;
+    if (!body.direction || !body.speed) {
+        res.status(400).json({ success: false,
+            message: "Request has to contain: direction, speed." });
+        return;
+    }
+    if (body.direction.toLowerCase() !== "left" && body.direction.toLowerCase() !== "right") {
+        res.status(400).json({ success: false,
+            message: `Direction has to be left or right! ${body.direction} is not allowed!` });
+        return;
+    }
+    if (body.speed > 49 || body.speed < 1) {
+        res.status(400).json({ success: false,
+            message: `Speed has to be between 1 and 49. ${body.speed} is not allowed!` });
+        return;
+    }
+
+    const speed = (body.direction.toLowerCase() === "left") ? 50 - body.speed : 50 + body.speed;
+    http.get({
+        host: "192.168.10.101",
+        path: `/cgi-bin/aw_ptz?cmd=%23P${speed}&res=1`,
+    }, (response) => {
+        if (response.statusCode === 200) {
+            res.status(200).json({ success: true,
+                message: `Camera moving ${body.direction} at speed ${body.speed}.` });
+        } else {
+            res.status(504).json({ success: false, message: "Unable to move camera." });
+        }
+    });
+});
+
+
+router.post("/tilt", (req, res) => {
+    const body = req.body;
+    if (!body.direction || !body.speed) {
+        res.status(400).json({ success: false,
+            message: "Request has to contain: direction, speed." });
+        return;
+    }
+    if (body.direction.toLowerCase() !== "up" && body.direction.toLowerCase() !== "down") {
+        res.status(400).json({ success: false,
+            message: `Direction has to be up or down! ${body.direction} is not allowed!` });
+        return;
+    }
+    if (body.speed > 49 || body.speed < 1) {
+        res.status(400).json({ success: false,
+            message: `Speed has to be between 1 and 49. ${body.speed} is not allowed!` });
+        return;
+    }
+
+    const speed = (body.direction.toLowerCase() === "down") ? 50 - body.speed : 50 + body.speed;
+    http.get({
+        host: "192.168.10.101",
+        path: `/cgi-bin/aw_ptz?cmd=%23T${speed}&res=1`,
+    }, (response) => {
+        if (response.statusCode === 200) {
+            res.status(200).json({ success: true,
+                message: `Camera moving ${body.direction} at speed ${body.speed}.` });
+        } else {
+            res.status(504).json({ success: false, message: "Unable to move camera." });
+        }
+    });
 });
 
 module.exports = router;
