f158fd07ca0084f354c52c8510d3e34788cea740
Bart de Jonge <B.A.P.deJonge@student.tudelft.nl>
2016-05-30 21:50:37 +0200

    trying to update timelnies when hovering users

--- elements/timeline-picker.js
+++ elements/timeline-picker.js
@@ -4,21 +4,33 @@ class timelinePicker {
 
         this.properties = {
             timelines: Object,
+            pickedTimelines: {
+                type: Array,
+                value: [],
+            },
+            hack: Object,
         };
     }
 
+    computeChecked(index) {
+        console.log(index)
+        console.log(this.pickedTimelines);
+        return this.pickedTimelines.indexOf(index) >= 0;
+    }
+
     buttonClicked() {
-        const toggles = document.querySelectorAll("paper-toggle-button");
-        const checkedToggles = [];
-
-        for (let i = 0; i < toggles.length; i++) {
-            if (toggles[i].checked) {
-                checkedToggles.push(toggles[i].value);
-            }
-        }
-
-        document.querySelector("#pickedTimelinesPost").body = { pickedTimelines: checkedToggles };
-        document.querySelector("#pickedTimelinesPost").generateRequest();
+        // const toggles = document.querySelectorAll("paper-toggle-button");
+        // const checkedToggles = [];
+        //
+        // for (let i = 0; i < toggles.length; i++) {
+        //     if (toggles[i].checked) {
+        //         checkedToggles.push(toggles[i].value);
+        //     }
+        // }
+        //
+        // document.querySelector("#pickedTimelinesPost").body = { pickedTimelines: checkedToggles };
+        // document.querySelector("#pickedTimelinesPost").generateRequest();
+        console.log(this.pickedTimelines);
     }
 
     handleResponse(event) {
@@ -26,11 +38,11 @@ class timelinePicker {
         this.timelines = event.detail.response.cameraTimelines;
     }
 
-    handlePickedTimelinesResponse(event) {
-        if (event.detail.response.success) {
-            window.location.href = "/timeline";
-        }
-    }
+    // handlePickedTimelinesResponse(event) {
+    //     if (event.detail.response.success) {
+    //         window.location.href = "/timeline";
+    //     }
+    // }
 }
 // eslint-disable-next-line
 Polymer(timelinePicker);
--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 import express from "express";
+import CameraOperator from "../objects/CameraOperator";
 const router = express.Router(); // eslint-disable-line new-cap
 
 router.post("/picked-user", (req, res) => {
@@ -6,6 +7,14 @@ router.post("/picked-user", (req, res) => {
     req.session.pickedUser = req.body.pickedUser;
 
     res.json({ success: true });
+});
+
+router.get("/get-users", (req, res) => {
+    res.json({ users: [
+        new CameraOperator(0, "Jan", []),
+        new CameraOperator(1, "Klaas", []),
+        new CameraOperator(2, "Piet", []),
+    ] });
 });
 
 module.exports = router;
