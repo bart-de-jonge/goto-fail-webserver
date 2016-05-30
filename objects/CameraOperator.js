c1fc06bc8edbce0dbcc31f7dd4e167c1db0cbdb1
Bart de Jonge <B.A.P.deJonge@student.tudelft.nl>
2016-05-30 11:22:20 +0200

    added users class

---
+++ objects/CameraOperator.js
@@ -0,0 +1,17 @@
+
+/*
+ * Class for storing a CameraOperator
+ */
+class CameraOperator {
+    constructor(id, name, pickedTimelines) {
+        this.id = id;
+        this.name = name;
+        this.pickedTimelines = pickedTimelines;
+    }
+
+    // static fromXML(XMLObject) {
+    //     // TODO: implement
+    // }
+}
+
+export default CameraOperator;
