e0b280b533a1131f86483e2cb5206a0904d939c5
Mark van de Ruit <markvanderuit@gmail.com>
2016-06-02 12:59:52 +0200

    - Styled drawer, in separate drawer class - Added links (with icons and fancy effects and stuff) to drawer for main views. - Restyled timeline headers to now be part of header bar. -

--- client_app/js/timeline.js
+++ client_app/js/timeline.js
@@ -2,16 +2,9 @@
 document.addEventListener("DOMContentLoaded", () => {
     const socket = io("/cameraOperators"); // eslint-disable-line no-undef
 
-    const header = document.getElementsByTagName("header")[0];
-    const dialog = document.createElement("paper-dialog");
-    dialog.id = "cur-count";
-    dialog.innerHTML = "";
-    header.appendChild(dialog);
-
     socket.on("next_count", (countData) => {
         // TODO: Add Data Manipulation Here
-        const newDialog = document.getElementById("cur-count");
-        newDialog.innerHTML = `The Current Count is ${countData.newCount}`;
+        console.log("Update the count");
 
         document.getElementsByTagName("timeline-grid")[0]
             .setAttribute("current-count", countData.newCount);
