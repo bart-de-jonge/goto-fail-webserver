e8cd4a287ae574683f02f2838f0373fd3d96ccb1
Alexander Geenen <geenen124@gmail.com>
2016-06-14 12:20:32 +0200

    New Object Tests

--- test/objects/ProjectManager.js
+++ test/objects/ProjectManager.js
@@ -18,12 +18,46 @@ describe("ProjectManager Creation", () => {
                 done();
             });
         });
+
+        it("Can Reload The Project Manager", done => {
+            ProjectManager.waitForXML(projectManager => {
+                projectManager.data = null;
+                projectManager.reloadProject((projectManager) => {
+                    expect(projectManager.data).to.not.be.null;
+                    done();
+                });
+            });
+        });
     } catch (e) {
-        console.log(e);
         // Then the file doesn't exist, so test for null data
         it("Should not fatally crash on missing project file", done => {
             ProjectManager.waitForXML(projectManager => {
                 expect(projectManager.data).to.be.null;
+                done();
+            });
+        });
+    } finally {
+        it("Can Handle Empty User XML", done => {
+            ProjectManager.waitForXML(projectManager => {
+                const userArray = projectManager.getUsersFromXML();
+                expect(userArray).to.eql([]);
+                done();
+            });
+        });
+
+        it("Can Parse Users From XML", done => {
+            const xmlObject = [
+                {
+                    user: [{
+                        chosenTimelines: [{ chosenTimeline: ["0", "1"] }],
+                        name: ["John"],
+                        roleValue: ["1"],
+                    }],
+                }
+            ];
+            ProjectManager.waitForXML(projectManager => {
+                const users = projectManager.getUsersFromXML(xmlObject);
+                expect(users).to.not.be.empty;
                 done();
             });
         });
