acdce7961779279aac38842089c1507f7bfb5a4e
Alexander Geenen <geenen124@gmail.com>
2016-06-15 21:39:59 +0200

    Add More Director Route Testing

--- test/routes/director.js
+++ test/routes/director.js
@@ -1,5 +1,8 @@
 import supertest from "supertest";
 import app from "../../app.js";
+import ProjectManager from "../../objects/ProjectManager";
+import DirectorTimeline from "../../objects/DirectorTimeline";
+import { expect } from "chai";
 
 const request = supertest(app);
 
@@ -8,5 +11,44 @@ describe("Routes: Director", () => {
         request.get("/director")
             .expect(200)
             .end(err => done(err));
+    });
+
+    it("GET /director/director-timeline", done => {
+        ProjectManager.waitForXML(projectManager => {
+            const tempData = projectManager.data;
+            const directorTimeline = new DirectorTimeline("The Director's Timeline");
+            projectManager.data = {
+                scriptingProject: {
+                    directorTimeline,
+                }
+            };
+
+            request.get("/director/director-timeline")
+                .expect(200)
+                .expect(res => {
+                    expect(res.body.success).to.be.true;
+                    expect(res.body.directorTimeline).to.deep.equal(directorTimeline);
+                })
+                .end(err => {
+                    projectManager.data = tempData;
+                    done(err);
+                });
+        });
+    });
+
+    it("GET /director/director-timeline", done => {
+        ProjectManager.waitForXML(projectManager => {
+            const tempData = projectManager.data;
+            projectManager.data = null;
+            request.get("/director/director-timeline")
+                .expect(200)
+                .expect(res => {
+                    expect(res.body.success).to.be.false;
+                })
+                .end(err => {
+                    projectManager.data = tempData;
+                    done(err);
+                });
+        });
     });
 });
