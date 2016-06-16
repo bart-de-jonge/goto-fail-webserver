76e02acd666402d002e163313b33e592d9d71618
Martijn Janssen <martijn9612@gmail.com>
2016-06-16 12:20:24 +0200

    Extract scrolling-box

--- client_app/js/director.js
+++ client_app/js/director.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const socket = io("/director"); // eslint-disable-line no-undef
 
     socket.on("next_count", (countData) => {
-        document.getElementsByTagName("director-view")[0]
+        document.getElementsByTagName("director-element")[0]
             .setAttribute("current-count", countData.newCount);
     });
 });
