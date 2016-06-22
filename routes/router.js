aa10c2486ff624a875ca1d79f9012ca015e5141d
Bart de Jonge <B.A.P.deJonge@student.tudelft.nl>
2016-06-22 11:59:26 +0200

    fixed failing etsts

--- routes/router.js
+++ routes/router.js
@@ -2,7 +2,6 @@ import index from "./index";
 import timeline from "./timeline";
 import uploadScp from "./upload-scp";
 import users from "./users";
-import presets from "./presets";
 import cameras from "./cameras";
 import shotCaller from "./shot-caller";
 import director from "./director";
@@ -15,7 +14,6 @@ module.exports.addRoutes = (app) => {
     app.use("/timeline", timeline);
     app.use("/upload-scp", uploadScp);
     app.use("/", users);
-    app.use("/presets", presets);
     app.use("/camera-control", cameraControl);
     app.use("/shot-caller", shotCaller);
     app.use("/director", director);
