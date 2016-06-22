5f02f3637c9f4ad96ece1dd0a6b4c499bfe52963
Alexander Geenen <geenen124@gmail.com>
2016-06-22 21:44:11 +0200

    Add Clarifying Comments

--- routes/sockets/sockets.js
+++ routes/sockets/sockets.js
@@ -24,10 +24,19 @@ const getMaxCount = callback => {
     });
 };
 
+/*
+ * Get a list of the current shots for the camera timelines.
+ * The index of a shot corresponds to the camera timeline's index
+ * @param currentCount The count to filter the shots by
+ * @param callback Callback called with the list of current shots as an argument
+ */
 const getCurrentShots = (currentCount, callback) => {
     ProjectManager.waitForXML(projectManager => {
         if (projectManager.data && projectManager.data.scriptingProject.cameraTimelines) {
             const cameraTimelines = projectManager.data.scriptingProject.cameraTimelines;
+
+            // Filter each timeline's shots based on the current count,
+            // and if none is current, it's current shot is null
             const currentShots = cameraTimelines.map(cameraTimeline => {
                 const matchingShots = cameraTimeline.getCameraShots()
                     .filter(shot =>
@@ -41,11 +50,20 @@ const getCurrentShots = (currentCount, callback) => {
 
             callback(currentShots);
         } else {
+            // If there isn't any data then there aren't any shots
             callback([]);
         }
     });
 };
 
+/*
+ * Compare the previous shots to the new camera shots.
+ * If the shots don't match, or a recall is forced, then recall the corresponding camera preset
+ * @param previousShots List of previous camera shots
+ * @param newShots List of new camera shots
+ * @param callback Called on successful completion of the recalls
+ * @param forceRecall Force the recalling of presents for new shots (even if they match)
+ */
 const compareAndRecallShots = (previousShots, newShots, callback, forceRecall) => {
     const benineHelper = new BenineHelper();
     newShots.forEach((newShot, index) => {
@@ -62,6 +80,10 @@ const compareAndRecallShots = (previousShots, newShots, callback, forceRecall) =
     callback();
 };
 
+/*
+ * Check whether or not the project is live.
+ * @param callback Callback called with whether or not the project is live as an argument.
+ */
 const checkIfLive = callback => {
     ProjectManager.waitForXML(projectManager => {
         callback(projectManager.live);
@@ -78,11 +100,13 @@ const listen = (server) => {
 
     let currentShots = [];
 
+    // Update the current shots, and recall presets if necessary (or if they are forced)
     const updateCurrentShots = (forceRecall = false) => {
-        logger.info("Updating the shots");
+        logger.info("Updating the current shots");
         getCurrentShots(currentCount, newShots => {
             checkIfLive(live => {
                 if (live) {
+                    // Project is live, so presets can be recalled
                     compareAndRecallShots(currentShots, newShots, () => {
                         currentShots = newShots;
                     }, forceRecall);
@@ -93,7 +117,8 @@ const listen = (server) => {
         });
     };
 
-    updateCurrentShots();
+    // Set the current shots, and force a recall if they're just being loaded in live
+    updateCurrentShots(true);
 
     // Watch Project File For Changes
     fs.watch(`${__dirname}/../../project-scp-files/`, (event, filename) => {
