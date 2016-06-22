c6b45281c1c8770d3028a29d49e3e2502f080085
Bart de Jonge <B.A.P.deJonge@student.tudelft.nl>
2016-06-22 11:32:24 +0200

    added coupled route

--- routes/benine.js
+++ routes/benine.js
@@ -51,13 +51,61 @@ router.post("/cameras/:id(\\d+)/set-remote-camera-id", (req, res) => {
         ProjectManager.waitForXML((manager) => {
             const camera = manager.data.scriptingProject.cameraList[0].camera[req.params.id];
             if (camera) {
+                camera.presetId = req.body.presetId;
+                res.json({ success: true, message: "Preset id stored successfully!" });
+            }
+            res.json({ success: false, message: "That's not a valid camera!" });
+        });
+    }
+    res.json({ success: false, message: "Please provide a remote camera id!" });
+});
+
+router.post("/cameras/:id(\\d+)/set-preset-id", (req, res) => {
+    if (req.body.presetId) {
+        ProjectManager.waitForXML((manager) => {
+            const camera = manager.data.scriptingProject.cameraList[0].camera[req.params.id];
+            if (camera) {
                 camera.remoteCameraId = req.body.remoteCameraId;
                 res.json({ success: true, message: "Remote camera id stored successfully!" });
             }
             res.json({ success: false, message: "That's not a valid camera!" });
         });
     }
-    res.json({ success: false, message: "Please prove a remote camera id!" });
+    res.json({ success: false, message: "Please provide a preset id!" });
+});
+
+router.get("/coupled", (req, res) => {
+    ProjectManager.waitForXML((manager) => {
+        const cameraList = manager.data.scriptingProject.cameraList[0].camera;
+        if (cameraList) {
+            let result = true;
+            cameraList.forEach((camera) => {
+                if (camera.remoteCameraId < 0) {
+                    result = false;
+                }
+            });
+            res.json({ success: true, coupled: result });
+        } else {
+            res.json({ success: false });
+        }
+    });
+});
+
+router.get("/coupled/:id(\\d+)", (req, res) => {
+    ProjectManager.waitForXML((manager) => {
+        const cameraList = manager.data.scriptingProject.cameraList[0].camera;
+        if (cameraList) {
+            let result = true;
+            cameraList.forEach((camera) => {
+                if (camera.remoteCameraId < 0) {
+                    result = false;
+                }
+            });
+            res.json({ success: true, coupled: result });
+        } else {
+            res.json({ success: false });
+        }
+    });
 });
 
 module.exports = router;
